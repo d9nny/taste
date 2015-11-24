9370454af6f53533b2c3238d9f6b61263647787e
blancopado <pablo.dominguezblanco@gmail.com>
2015-11-24 12:58:53 +0100

    editing restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,20 +1,35 @@
 class RestaurantsController < ApplicationController
 
-def create
-	Restaurant.create(restaurant_params)
-	redirect_to '/restaurants'
-end 
+	def create
+		@restaurant = Restaurant.create(restaurant_params)
+		redirect_to '/restaurants'
+	end 
 
-def index
-	@restaurants = Restaurant.all
-end
+	def index
+		@restaurants = Restaurant.all
+	end
 
-def new
-	puts params
-end
+	def new
+		@restaurant = Restaurant.new
+	end
 
-def restaurant_params
-  params.require(:restaurant).permit(:name)
-end
+	def restaurant_params
+	  params.require(:restaurant).permit(:name)
+	end
+
+	def show
+	  @restaurant = Restaurant.find(params[:id])
+	end
+
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+
+    redirect_to '/restaurants'
+  end
 
 end
