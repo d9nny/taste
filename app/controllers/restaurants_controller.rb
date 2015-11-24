fd5296a7e08ef27cdd771a27d272f7beae1c1c81
blancopado <pablo.dominguezblanco@gmail.com>
2015-11-24 12:29:02 +0100

    new restaurants view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,20 @@
 class RestaurantsController < ApplicationController
 
+def create
+	Restaurant.create(restaurant_params)
+	redirect_to '/restaurants'
+end 
+
 def index
-	'Hello from the index action'
+	@restaurants = Restaurant.all
+end
+
+def new
+	puts params
+end
+
+def restaurant_params
+  params.require(:restaurant).permit(:name)
 end
 
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -11,10 +11,9 @@ feature 'restaurants' do
   end
 
   context 'restaurants have been added' do
-  before do
-    Restaurant.create(name: 'KFC')
-  end
-
+	  before do
+	    Restaurant.create(name: 'KFC')
+	  end
   scenario 'display restaurants' do
     visit '/restaurants'
     expect(page).to have_content('KFC')
@@ -22,5 +21,15 @@ feature 'restaurants' do
   end
 	end
 
+	context 'creating restaurants' do
+  scenario 'prompts user to fill out a form, then displays the new restaurant' do
+    visit '/restaurants'
+    click_link 'Add a restaurant'
+    fill_in 'Name', with: 'KFC'
+    click_button 'Create Restaurant'
+    expect(page).to have_content 'KFC'
+    expect(current_path).to eq '/restaurants'
+  end
+end
 
 end
