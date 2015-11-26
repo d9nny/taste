d43a70d270664d3bfea26b0a4490e73d89ba1285
jamierose <jrose111284@gmail.com>
2015-11-26 16:54:54 +0000

    test endorsements

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,7 @@ class Review < ActiveRecord::Base
   belongs_to :user
   validates :rating, inclusion: (1..5)
   validates :user, uniqueness: {scope: :restaurant, message: "Only one review per restaurant"}
+  has_many :endorsements
 
   def get_time
   	return created_at
